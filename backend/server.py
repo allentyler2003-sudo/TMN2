7a5b3bceb9ec6ff957ac77dd02e029b06894944f
allentyler2003-sudo <allentyler2003@gmail.com>
2026-09-14 10:41:04 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -258,10 +258,10 @@ async def login(request: Request, response: Response):
         )
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    await db.login_attempts.delete_many({"identifier": identifier})
+        await db.login_attempts.delete_many({"identifier": identifier})
     access_token = create_token(str(user["_id"]), "access")
-refresh_token = create_token(str(user["_id"]), "refresh")
-set_auth_cookies(response, access_token, refresh_token)
+    refresh_token = create_token(str(user["_id"]), "refresh")
+    set_auth_cookies(response, access_token, refresh_token)
     return public_user(user)
     
 
